cb20532550af1579a1fe460a13e0e15e2c41a3d5
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-03-25 16:42:06 +0600

    design add note screes

--- lib/pages/add_note.dart
+++ lib/pages/add_note.dart
@@ -14,16 +14,40 @@ class _AddNewNoteState extends State<AddNewNote> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          TextField(
-            style: TextStyle(
-              fontSize: 20.sp,
-            ),
-            decoration: InputDecoration(hintText: "Title"),
-          ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.check),
+          )
         ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+        child: Column(
+          children: [
+            TextField(
+              autofocus: true,
+              textInputAction: TextInputAction.next,
+              style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+              decoration: InputDecoration(
+                hintText: "Title",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 5.h),
+            TextField(
+              maxLines: null,
+              style: TextStyle(
+                fontSize: 15.sp,
+              ),
+              decoration: InputDecoration(
+                hintText: "Notes",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
